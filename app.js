8477a40f2d45fbccdb651c24df7ab1bf64502856
dantasyisland <dantothelastdrop@yahoo.com>
2022-01-12 01:32:30 -0600

    messed with json

--- app.js
+++ app.js
@@ -3,16 +3,16 @@ const app = express();
 const data = require('./data.json');
 const routes = require('./routes/index');
 
+app.use(express.json);
 
 app.set('view engine', 'pug');
 
 app.use(express.static("public"));
 
-console.log(data);
-
 
 app.use(routes);
 
-
 app.listen(3000);
 console.log("App is listening on port 3000");
+
+// sudo killall -9 node
--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,34 @@
 const express = require('express');
 const router = express.Router();
+const data = require('../data.json')
 
 router.get("/", (req, res) => {
+  res.render("index");
   console.log("Hey you made it!");
   // res.send('WELCOME TO JURASSIC PARK');
-  res.render("index")
+  // res.locals = data.projects;
 });
 
 router.get("/about", (req, res) => {
   // console.log("About the about...");
   // res.send('Abouty bouty and rowdy rowdy')
-  res.render("about")
+  res.render("about");
 })
 
 router.get("/projects/:id", (req, res) => {
   res.send(`${req.params.id}`)
   console.log(`${req.params.id}`);
+  console.log(`${data}`);
 })
 
 
+// function KickOutTheJams(data) {
+//   data.projects.forEach(element => {
+//     console.log(element);
+//   });
+// }
+
+// KickOutTheJams(data);
 
 
 module.exports = router;
