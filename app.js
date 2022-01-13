b732e1f4c0139cd5e3c97eb15675b8dd7076fcfd
dantasyisland <dantothelastdrop@yahoo.com>
2022-01-13 10:47:09 -0600

    playing with json

--- app.js
+++ app.js
@@ -3,13 +3,12 @@ const app = express();
 const data = require('./data.json');
 const routes = require('./routes/index');
 
-app.set('view engine', 'pug');
+
 
 app.use(routes);
 
 // app.use(express.static("public"));
-// app.use(express.json);
-
+// app.use(express.json)
 
 app.listen(3000);
 console.log("App is listening on port 3000");
