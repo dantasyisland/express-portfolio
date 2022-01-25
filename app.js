676f97d58126e3b93861a808a9f811e4412e4793
dantasyisland <dantothelastdrop@yahoo.com>
2022-01-25 01:31:51 -0600

    Almost done! Crop pics - add descriptions - css

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
-const {projects} = require("./data.json");
+const {
+  projects
+} = require("./data.json");
 
 const errorHandler = require("./errorHandler");
 
@@ -13,13 +15,12 @@ app.set("view engine", "pug");
  * Middleware for parsing JSON, setting static route
  */
 app.use(express.json());
-app.use("/static", express.static("public"));
+app.use("/public", express.static("public"));
 
 /**
  * Route for index page - passing project data to be rendered
  */
 app.get("/", (req, res, next) => {
-
   res.render("index", {
     projects,
   });
@@ -33,8 +34,8 @@ app.get("/about", (req, res, next) => {
 });
 
 app.get("/projects/:id", (req, res, next) => {
-  const project = projects[req.params.id]
-  if (req.params.id> projects.length || isNaN(req.params.id) ) {
+  const project = projects[req.params.id];
+  if (req.params.id > projects.length || isNaN(req.params.id)) {
     const err = new Error(`Whoopsidoodle! Looks like that project doesn't exist`);
     err.status = 404;
     next(err);
@@ -45,23 +46,26 @@ app.get("/projects/:id", (req, res, next) => {
 
 /** Error Handlers */
 app.use((req, res, next) => {
-  const err = new Error(`Whoops! Looks like that page doesn't exist`);
+  const err = new Error(`Whoopsidoodle! Looks like that page doesn't exist`);
   err.status = 404;
   res.render("page-not-found", {
     err,
   });
 });
-app.use((err, req, res, next) => {
-  if (err.status === 500) {
-    err.message = `It's not you. It's us! Something went wrong on the server. Sorry! Please try again!`
-  }
-  console.error(err.message);
-  console.error(err.status)
-  res.render('errorPage', {
-    err
-  });
-});
 
+app.use((err, req, res, next) => {
+  if (!err.status) {
+    err.message = `It's not you. It's us! Something went wrong on the server. Sorry! Please try again!`;
+    err.status = 500;
+    res.render("errorPage", {
+      err,
+    });
+  } else {
+    res.render("errorPage", {
+      err
+    });
+  }
+});
 
 /**
  * Starts server
